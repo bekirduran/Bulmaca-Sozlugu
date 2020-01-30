f010ca84c37720212e1dc2e89c6ff0f3673bf174
Bekir Duran <akinmihal@gmail.com>
2020-01-30 09:31:02 +0300

    Initial Commit

---
+++ Bulmaca Sozlugu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bulmaca Sozlugu
+//
+//  Created by Bekir Duran on 30.01.2020.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
